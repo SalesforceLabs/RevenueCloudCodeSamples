f9fcbe9ee390f35917d59690b09a3cbf27452cd3
Brian Galdino <bgaldino@salesforce.com>
2022-12-16 08:57:28 -0800

    amend support and component button bug fix

--- force-app/main/default/lwc/assetManagementRelated/assetManagementRelated.js
+++ force-app/main/default/lwc/assetManagementRelated/assetManagementRelated.js
@@ -1,103 +1,149 @@
-import { LightningElement, wire, track, api} from 'lwc';
-import getAssets from '@salesforce/apex/AssetManagementController.getAssetsByAccount'
-import renewAssets from '@salesforce/apex/AssetManagementController.renewAssets'
-import cancelAssets from '@salesforce/apex/AssetManagementController.cancelAssets'
+import { LightningElement, wire, track, api } from "lwc";
+import getAssets from "@salesforce/apex/AssetManagementController.getAssetsByAccount";
+import renewAssets from "@salesforce/apex/AssetManagementController.renewAssets";
+import cancelAssets from "@salesforce/apex/AssetManagementController.cancelAssets";
+import amendAssets from "@salesforce/apex/AssetManagementController.amendAssets";
 
 const columns = [
-    { label: 'Asset Name', fieldName: 'Name', type: 'text' },
-    { label: 'Lifecycle Start Date', fieldName: 'LifecycleStartDate',type: 'date', typeAttributes: {
-        day: "2-digit",
-        month: "2-digit",
-        year:"numeric",
-        timeZone:"UTC"
-    }},
-    { label: 'Lifecycle End Date', fieldName: 'LifecycleEndDate', type: 'date', typeAttributes: {
-        day: "2-digit",
-        month: "2-digit",
-        year:"numeric",
-        timeZone:"UTC"
-    }},
-    { label: 'Renewal Term Unit', fieldName: 'RenewalTermUnit', type: 'text'},
-    { label: 'Renewal Term', fieldName: 'RenewalTerm', type: 'Number'},
+    { label: "Asset Name", fieldName: "Name", type: "text" },
+    {
+        label: "Lifecycle Start Date",
+        fieldName: "LifecycleStartDate",
+        type: "date",
+        typeAttributes: {
+            day: "2-digit",
+            month: "2-digit",
+            year: "numeric",
+            timeZone: "UTC"
+        }
+    },
+    {
+        label: "Lifecycle End Date",
+        fieldName: "LifecycleEndDate",
+        type: "date",
+        typeAttributes: {
+            day: "2-digit",
+            month: "2-digit",
+            year: "numeric",
+            timeZone: "UTC"
+        }
+    },
+    { label: "Renewal Term Unit", fieldName: "RenewalTermUnit", type: "text" },
+    { label: "Renewal Term", fieldName: "RenewalTerm", type: "Number" }
 ];
-
-
 
 export default class AssetManagement extends LightningElement {
     isButtonsActivated = true;
     isCancelDatePopup = false;
+    isAmendDatePopup = false;
     @track assetList;
     @track error;
-    @api recordId
+    @api recordId;
     @track columns = columns;
     selectedRows = [];
-    @track cancelledDate
+    @track cancelledDate;
+    @track amendedDate;
+    @track amendQuantity;
     @track isLoaded = false;
 
-    @wire(getAssets , {accountId : '$recordId'})
-    assets({error, data}){
-        if(data){
+    @wire(getAssets, { accountId: "$recordId" })
+    assets({ error, data }) {
+        if (data) {
             this.assetList = data;
-        }
-        else if(error){
+        } else if (error) {
             this.error = error;
             this.assetList = undefined;
         }
     }
 
-    handleRowSelection = event => {
-        this.selectedRows =event.detail.selectedRows;
+    handleRowSelection = (event) => {
+        this.selectedRows = event.detail.selectedRows;
         console.log(this.selectedRows);
-        this.isButtonsActivated =  this.selectedRows.length > 0 ?  false : true;
-    }
+        this.isButtonsActivated = this.selectedRows.length > 0 ? false : true;
+    };
 
     toggleCancelDatePopup = () => {
-        this.isCancelDatePopup = this.isCancelDatePopup === false ? true : false
-    }
+        this.isCancelDatePopup = this.isCancelDatePopup === false ? true : false;
+    };
 
-    handleAction = event => {
+    toggleAmendDatePopup = () => {
+        this.isAmendDatePopup = this.isAmendDatePopup === false ? true : false;
+    };
+
+    handleAction = (event) => {
         let actionType = event.currentTarget.name;
 
         this.isLoaded = true;
-        console.log('actionType',actionType);
-        if(actionType === 'Renew') {
+        console.log("actionType", actionType);
+        if (actionType === "Renew") {
             this.handleRenewAssets();
-        }
-        else if(actionType === 'Cancel') {
+        } else if (actionType === "Cancel") {
             this.handleCancelAssets();
+        } else if (actionType === "Amend") {
+            this.handleAmendAssets();
         }
         this.isLoaded = false;
         this.isCancelDatePopup = false;
-    }
+        this.isAmendDatePopup = false;
+    };
 
+    handleRenewAssets = () => {
+        renewAssets({ assetList: this.selectedRows })
+            .then((data) => {
+                console.log(data);
+            })
+            .catch((error) => {
+                this.error = error;
+            });
+    };
 
-    handleRenewAssets = () =>{
-        renewAssets({assetList : this.selectedRows})
-        .then((data) => {
-            console.log(data);
+    handleCancelAssets = () => {
+        cancelAssets({
+            assetList: this.selectedRows,
+            cancelDate: this.cancelledDate
         })
-        .catch((error) => {
-            this.error = error;
-        });
-    }
-
-    handleCancelAssets = () =>{
-        cancelAssets({assetList : this.selectedRows, cancelDate: this.cancelledDate})
-        .then((data) => {
-            console.log(data);
-        })
-        .catch((error) => {
-            this.error = error;
-        });
+            .then((data) => {
+                console.log(data);
+            })
+            .catch((error) => {
+                this.error = error;
+            });
         this.isCancelDatePopup = false;
-    }
+    };
 
-    handleDate(event){
+    handleAmendAssets = () => {
+        amendAssets({
+            assetList: this.selectedRows,
+            startDate: this.amendedDate,
+            quantityChange: this.amendedQuantity
+        })
+            .then((data) => {
+                console.log(data);
+            })
+            .catch((error) => {
+                this.error = error;
+            });
+        this.isAmendDatePopup = false;
+    };
+
+    handleDate(event) {
         this.cancelledDate = event.currentTarget.value;
+        this.amendedDate = event.currentTarget.value;
     }
 
-    get toggleCancelAssetButton(){
-        return this.selectedRows.length > 0 && this.cancelledDate !== undefined ?  false : true;
+    handleQuantity(event) {
+        this.amendedQuantity = event.currentTarget.value;
     }
 
+    get toggleCancelAssetButton() {
+        return this.selectedRows.length > 0 && this.cancelledDate !== undefined
+            ? false
+            : true;
+    }
+
+    get toggleAmendAssetButton() {
+        return this.selectedRows.length > 0 && this.amendedDate !== undefined
+            ? false
+            : true;
+    }
 }

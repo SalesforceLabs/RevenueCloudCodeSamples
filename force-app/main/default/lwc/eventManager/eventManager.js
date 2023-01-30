9d8073f3e091ffa0312572342a77ae4728290efe
Pramodhkumar Thammishetty <p.thammishetty@salesforce.com>
2023-01-30 12:18:22 -0800

    Cleanup

--- force-app/main/default/lwc/eventManager/eventManager.js
+++ force-app/main/default/lwc/eventManager/eventManager.js
@@ -28,22 +28,8 @@ export default class EventManager extends LightningElement {
         const thisReference = this;
         const messageCallback = function(response) {
             let obj = JSON.parse(JSON.stringify(response));
-            console.log('<<<<>>>>>'+JSON.stringify(obj));
             if(currentUserId === obj.data.payload.CreatedById){ //dispatch the users events instead of all events
-                /*
-                let message = 'Order created: ' + obj.data.payload.CancellationRecordId;
-
-                let variant = 'success'
-                if (obj.data.payload.AssetCancelErrorDetailEvents != null) {
-                    message = 'Couldn’t cancel the asset: ' + obj.data.payload.AssetCancelErrorDetailEvents[0].ErrorMessage;
-                    variant = 'error'
-                }
-                const evt = new ShowToastEvent({
-                    message: message,
-                    variant: variant,
-                    mode : "sticky"
-                });
-                */
+                
                 thisReference.dispatchEvent(new CustomEvent('custevent', { detail: obj }));
             }
             // Response contains the payload of the new message received
@@ -65,20 +51,7 @@ export default class EventManager extends LightningElement {
             let obj = JSON.parse(JSON.stringify(response));
             
             if(currentUserId === obj.data.payload.CreatedById){ //dispatch the users events instead of all events
-                /*
-
-                let message = 'Order created: ' + obj.data.payload.RenewalRecordId;
-                let variant = 'success'
-                if (obj.data.payload.AssetRenewErrorDetailEvents != null) {
-                    message = 'Couldn’t renew the asset: ' + obj.data.payload.AssetRenewErrorDetailEvents[0].ErrorMessage;
-                    variant = 'error'
-                }
-                const evt = new ShowToastEvent({
-                    message: message,
-                    variant: variant,
-                    mode : "sticky"
-                });*/
-
+                
                 thisReference.dispatchEvent(new CustomEvent('custevent', { detail: obj }));
             }
             // Response contains the payload of the new message received
